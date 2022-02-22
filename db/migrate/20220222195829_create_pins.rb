65451ab56a65f18a97fa99632bed9f020d3d7f00
Josh Thompson <thompsonjoshd@gmail.com>
2022-02-22 16:07:35 -0600

    added Users scaffold, nav bar

--- db/migrate/20220222195829_create_pins.rb
+++ db/migrate/20220222195829_create_pins.rb
@@ -3,11 +3,11 @@ class CreatePins < ActiveRecord::Migration[7.0]
     create_table :pins do |t|
       t.string :title
       t.text :description
-      t.string :latitude
-      t.string :float
+      t.float :latitude
       t.float :longitude
 
       t.timestamps
     end
   end
 end
+
--- db/schema.rb
+++ db/schema.rb
@@ -17,8 +17,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_22_212538) do
   create_table "pins", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.string "latitude"
-    t.string "float"
+    t.float "latitude"
     t.float "longitude"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
